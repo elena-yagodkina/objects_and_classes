2b7006993c1faee2b45a9184b48cabdb585bc190
elena_yagodkina <engineerlena@mail.ru>
2024-01-22 17:23:51 +0300

    homework

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,25 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Author kathleenRowling = new Author("Kathleen", "Rowling");
-        System.out.println(kathleenRowling.getFirstName() + " " + kathleenRowling.getLastName());
+        System.out.println(kathleenRowling);
 
         Author stephenieMeyer = new Author("Meyer", "Stephenie");
-        System.out.println(stephenieMeyer.getFirstName() + " " + stephenieMeyer.getLastName());
+        System.out.println(stephenieMeyer);
 
         Book bookOne = new Book("Garry Potter", kathleenRowling, 1997);
-        System.out.println(bookOne.getTitle() + ", " + bookOne.getNameAuthor().getFirstName() + ", " + bookOne.getYearPublishing());
+        System.out.println(bookOne);
 
         Book bookTwo = new Book("Twilight", stephenieMeyer, 2005);
-        System.out.println(bookTwo.getTitle() + ", " + bookTwo.getNameAuthor().getFirstName() + ", " + bookTwo.getYearPublishing());
+        System.out.println(bookTwo);
 
         bookOne.setYearPublishing(1998);
         System.out.println(bookOne.getYearPublishing());
 
+        System.out.println(bookOne.equals(bookTwo));
+        System.out.println(bookOne.hashCode());
+        System.out.println(bookTwo.hashCode());
+        System.out.println(kathleenRowling.equals(stephenieMeyer));
+        System.out.println(kathleenRowling.hashCode());
+        System.out.println(stephenieMeyer.hashCode());
     }
 }
